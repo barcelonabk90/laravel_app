6accea7220c7fb02b8e0426106c57a557eaec628
barcelonabk90 <buiquangbk90@gmail.com>
2018-12-18 11:31:55 +0900

    setting url

---
+++ laravel_app/html/abctest/config.php
@@ -0,0 +1,11 @@
+<?php
+define('SPAYCIAL_KAISYACD', '1000164'); //会社コード
+define('SPAYCIAL_KAISYA_URL','abctest'); //会社URL
+define('SPAYCIAL_LANGUAGE','ja');       //言語
+//DB関連
+define('SPAYCIAL_DB_HOST'    ,'localhost');  //ホスト
+define('SPAYCIAL_DB_USERNAME','web_abctest'); //ユーザ名
+define('SPAYCIAL_DB_PASSWORD','spayabctest'); //パスワード
+define('SPAYCIAL_DB_DBNAME'  ,'abctest'); //DB名
+define('AGREE_TEXT'  ,'　給与所得の源泉徴収票及び給与等の支払明細書に記載すべき事を電磁的方法により提供する場合は、受給者の承諾が必要となります。
+<div>　承諾しない場合は、当システムをご利用いただけません。</div>');
---
+++ laravel_app/html/abctest/index.php
@@ -0,0 +1,4 @@
+<?php
+define('LANG_INI_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR . 'languages');
+require_once 'config.php';
+require_once '../../public/index.php';
